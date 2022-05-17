fe60378661fcbbf1518b32ac0b08829a316e52d9
Carlos <carlos.anezeh@gmail.com>
2022-05-17 17:30:55 -0500

    Create random number for player game
    Co-authored-by Ever <ever864@gmail.com>
    Co-authored-by AgustinPalmaM <palma.agustin7@gmail.com>

--- lib/board.rb
+++ lib/board.rb
@@ -1,18 +1,17 @@
 class Board
-  def initialize(trie_one, trie_two)
-    @trie_one = trie_one
-    @trie_two = trie_two
+
+  def initialize()
+    @trie_one = nil
+    @trie_two = nil
     @total = 0
     @board = nil
   end
 
   def generate_board
-    @board = Array.new(10) {Array.new(3) { 0 }}
+    @board = Array.new(10) { Array.new(3) { '|__' } }
   end
 
   def total_score
     @total = @trie_one + @trie_two
   end
-
 end
-
--- lib/game.rb
+++ lib/game.rb
@@ -2,14 +2,14 @@ require_relative 'player'
 require_relative 'board'
 
 class Game
+  attr_reader :board
+
   def initialize
     @player = Player.new
     @game = 10
     @pins = 10
     @score = 0
   end
-
-	
 
   def start
 		@player.get_name
@@ -19,7 +19,19 @@ class Game
 	def rounds
 		round_one = rand(11)
 		round_two = rand(11 - round_one)
+
+    return ["Player: #{@player.name} \nRound 1: #{round_one}", "Round 2: #{round_two}"]
 	end
+
+  # def display_board
+  #   board = Board.new.generate_board
+  #   board.map do |b|
+  #     b.map do |c|
+  #       print c
+  #     end
+  #     print "\n"
+  #   end
+  # end
 end
 
 new_game = Game.new
